5cb148e066b1b4b156e6fb98de4ed1e70d6785ce
Sachin Srivastava <sachinsrivastava.id@gmail.com>
2025-07-25 05:57:12 +0000

    i cant see the image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
@@ -652,3 +652,5 @@ export default function NirogyamPage() {
         </div>
     );
 }
+
+    
