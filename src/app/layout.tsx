75ae06731346afb52097e3fcaaf91725401f3225
Sachin Srivastava <sachinsrivastava.id@gmail.com>
2025-07-27 17:06:34 +0000

    i wnat to integrate a AI Chatbot Named norogyam chat bot which appears o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { cn } from '@/lib/utils';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Nirogyam - Kidney Health Education',
@@ -19,10 +20,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <Script async type='module' src='https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js'></Script>
       </head>
       <body className={cn("min-h-screen font-sans antialiased")}>
         {children}
         <Toaster />
+        <zapier-interfaces-chatbot-embed is-popup='true' chatbot-id='cmdjl8il0001prscdldia2w3v'></zapier-interfaces-chatbot-embed>
       </body>
     </html>
   );
